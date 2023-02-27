7fed0ef5c99f468a52c93478b4c3c9f37d1d83b3
kaiyuanZhu123 <3426147727@qq.com>
2023-02-27 17:39:04 +0800

    homework

---
+++ ActivityDemo/openCV/build/intermediates/cxx/Debug/1n2f24t2/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"S:\\Android_Studio_SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Demo\\android-intro-master\\starter\\ActivityDemo\\openCV\\.cxx\\Debug\\1n2f24t2\\x86" ^
+  opencv_jni_shared
---
+++ ActivityDemo/openCV/build/intermediates/cxx/Debug/1n2f24t2/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"S:\\Android_Studio_SDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\Demo\\android-intro-master\\starter\\ActivityDemo\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=S:\\Android_Studio_SDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=S:\\Android_Studio_SDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=S:\\Android_Studio_SDK\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=S:\\Android_Studio_SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\Demo\\android-intro-master\\starter\\ActivityDemo\\openCV\\build\\intermediates\\cxx\\Debug\\1n2f24t2\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\Demo\\android-intro-master\\starter\\ActivityDemo\\openCV\\build\\intermediates\\cxx\\Debug\\1n2f24t2\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\Demo\\android-intro-master\\starter\\ActivityDemo\\openCV\\.cxx\\Debug\\1n2f24t2\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
